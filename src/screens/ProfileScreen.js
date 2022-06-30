43d81c0662180532e15bd331f34655ae02341234
Tesfa-Worku <hopegorg@gmail.com>
2022-06-29 21:27:54 -0400

    refactor(profile): add header for profileScreen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,39 @@
-import { View, Text} from "react-native";
+import { View, Text, SafeAreaView, TouchableOpacity, ImageBackground} from "react-native";
 
-const ProfileScreen = () => {
+const ProfileScreen = ({navigation}) => {
     return (
-        <View style = {{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Profile Screen</Text>            
-        </View>
+        <SafeAreaView 
+            style = {{ 
+                flex: 1, 
+                justifyContent: "space-between", 
+                alignItems: 'stretch'
+            }}
+        >
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    marginTop: 40,
+                }}
+            >
+                <Text 
+                    style={{
+                        fontSize: 18, 
+                    }}
+                >
+                    Profile
+                </Text>
+                <TouchableOpacity 
+                    onPress={() => navigation.openDrawer()}
+                >
+                    <ImageBackground
+                        source={require('../../assets/user-profile.jpg')}
+                        style={{width: 35, height: 35}}
+                        imageStyle={{borderRadius: 25}}
+                    />
+                </TouchableOpacity>
+            </View>            
+        </SafeAreaView>
     )
 };
 
